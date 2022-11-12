8896bf358efd41c9efabbbf2b180b1f6c24d6fec
Jang A Reum <jangareum03@naver.com>
2022-11-12 10:50:08 +0900

    [추가] 수입/지출화면에서 '고정/변동', '금액', '사진' 기능 구현(#26) 1. 고정/변동 영역에서 고정 선택 시 옵션 메뉴 표시     - 옵션메뉴는 '일년, 한달, 일주일' 표시     - 변동 재선택 시 옵션메뉴 미표시 및 선택한 옵션메뉴 해제 2. 금액 영역에서 숫자만 입력 가능     - 숫자 제외한 문자 입력 시 자동 삭제 3. 사진 영역에서 선택한 사진 표시     - 선택한 사진이 수입/지출 화면의 사진 영역에 미리보기로 표시     - 선택한 사진 취소 시 기본 이미지로 변경되어 표시

--- src/main/resources/static/js/write.js
+++ src/main/resources/static/js/write.js
@@ -2,3 +2,25 @@ function openMapPop(){
     let option = 'top=320, left=660, width=800, height=500, status=no, menubar=no, toolbar=no, resizable=no';
     window.open('/write/map', '팝업', option);
 }
+
+function showOption(){
+    let fix = document.getElementById('fix');
+    let cycle = document.getElementsByClassName('fixCycle');
+
+    if( fix.checked ) {
+        document.getElementById('fixOption').style.display = 'block';
+    }else{
+        document.getElementById('fixOption').style.display = 'none';
+        for( let i=0; i<cycle.length; i++ ) {
+            cycle[i].checked = false;
+        }
+    }
+}
+
+function checkPrice( target ){
+    const pattern = /[^0-9]/g;
+
+    if( pattern.test(target.value) ) {
+        target.value = target.value.replace(pattern, '');
+    }
+}
