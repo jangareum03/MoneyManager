3bcfe633bc3fc310297dac7d0290dd6a49ff298d
jangareum03 <jangreum03@gmail.com>
2026-03-30 00:23:03 +0900

    feat: LedgerImage 생성 시 유효성 검증 로직 추가 - 경로 저장 시 OS 의존성 제거 - 정적 팩토리 메서드로 객체 생성 가능 - 생성 시 입력값 검증 로직 추가 - 검증 실패 시 BusinessException 발생 - 객체 생성 시점에 데이터 무결성 보장하도록 개선

--- src/main/java/com/moneymanager/domain/ledger/entity/LedgerImage.java
+++ src/main/java/com/moneymanager/domain/ledger/entity/LedgerImage.java
@@ -1,9 +1,13 @@
 package com.moneymanager.domain.ledger.entity;
 
+import com.moneymanager.exception.BusinessException;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+
+import static com.moneymanager.exception.error.ErrorCode.*;
+import static com.moneymanager.utils.validation.ValidationUtils.isNullOrBlank;
 
 /**
  * <p>
@@ -36,11 +40,84 @@ import java.time.LocalDateTime;
 @Getter
 public class LedgerImage {
 	private Long id;										//이미지 식별번호
-	private Long ledgerId;							//가계부 시스템 고유번호
+	private Long ledgerId;							//가계부 시스템 고유번호(내부용)
 
 	private String imagePath;						//이미지 상대경로
 	private int sortOrder;							//나열 순서
 
 	private LocalDateTime createdAt;		//등록일
 	private LocalDateTime updatedAt;		//수정일
+
+	/**
+	 *	가계부 이미지 정보를 검증하고, {@link LedgerImage} 객체를 생성합니다.
+	 *<p>
+	 *     검증할 항목:
+	 *     <ul>
+	 *         <li>가계부 ID 유효성</li>
+	 *         <li>이미지 경로 유효성</li>
+	 *         <li>정렬 순서 값 유효성</li>
+	 *     </ul>
+	 *</p>
+	 * 검증 실패 시 {@link BusinessException}가 발생합니다.
+	 *
+	 * @param ledgerId	가계부 ID
+	 * @param path	이미지 저장한 상대경로
+	 * @param order 이미지 정렬 순서
+	 * @return	검증된 정보를 기반으로 생성된 {@link LedgerImage} 객체
+	 * @throws BusinessException	입력값이 유효하지 않은 경우 발생
+	 */
+	public static LedgerImage create(Long ledgerId, String path, int order) {
+		String dbPath = path.replace("\\", "/");
+
+		validateLedgerId(ledgerId);
+		validateOrder(order);
+		validatePath(dbPath);
+
+		return LedgerImage.builder()
+				.ledgerId(ledgerId)
+				.imagePath(dbPath)
+				.sortOrder(order)
+				.build();
+	}
+
+	private static void validateLedgerId(Long id) {
+		if(id == null) {
+			throw BusinessException.of(
+					LEDGER_TARGET_MISSING,
+					"가계부 이미지 검증 실패   |   reason=필수값누락   |   object=LedgerImage   |   field=ledgerId   |   value=" + id
+			);
+		}
+
+		if(id < 1) {
+			throw BusinessException.of(
+					LEDGER_TARGET_RANGE,
+					"가계부 이미지 검증 실패   |   reason=범위오류   |   object=LedgerImage   |   field=ledgerId   |   min=1   |   value=" + id
+			);
+		}
+	}
+
+	private static void validatePath(String path) {
+		if(isNullOrBlank(path)) {
+			throw BusinessException.of(
+					LEDGER_TARGET_MISSING,
+					"가계부 이미지 검증 실패   |   reason=필수값누락   |   object=LedgerImage   |   field=imagePath   |   value=" + path
+			);
+		}
+
+		if(path.contains("\\")) {
+			throw BusinessException.of(
+					LEDGER_TARGET_FORMAT,
+					"가계부 이미지 검증 실패   |   reason=형식오류   |   object=LedgerImage   |   field=imagePath   |   expectedFormat=연도/월/이미지명.확장자   |   value=" + path
+			);
+		}
+	}
+
+	private static void validateOrder(int order) {
+		if(!(0 < order && order < 4)) {
+			throw BusinessException.of(
+					LEDGER_TARGET_RANGE,
+					"가계부 이미지 검증 실패   |   reason=범위오류   |   object=LedgerImage   |   field=sortOrder   |   min=1   |   max=3   |   value=" + order
+			);
+		}
+	}
 }
