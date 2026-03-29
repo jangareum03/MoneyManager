7c4b5ab472cf782fd856a9551acc57eca5496e7b
jangareum03 <jangreum03@gmail.com>
2026-03-30 00:57:18 +0900

    feat: BusinessException 예외 클래스 및 ServiceAction 추가 - 생성자로 외부에서 접근 불가하게 정적 생성 메서드 추가 - 에러 코드 및 ID 조회 기능 추가 - 서비스 액션 및 사용자 메시지 변경 기능 추가

---
+++ src/main/java/com/moneymanager/exception/error/ServiceAction.java
@@ -0,0 +1,42 @@
+package com.moneymanager.exception.error;
+
+import lombok.Getter;
+
+/**
+ * <p>
+ * 패키지이름    : com.moneymanager.exception.error<br>
+ * 파일이름       : ServiceAction<br>
+ * 작성자          : areum Jang<br>
+ * 생성날짜       : 26. 3. 6<br>
+ * 설명              : 서비스 요청 상황을 코드로 정의한 클래스
+ * </p>
+ * <br>
+ * <p color='#FFC658'>📢 변경이력</p>
+ * <table border="1" cellpadding="5" cellspacing="0" style="width: 100%">
+ * 		<thead>
+ * 		 	<tr style="border-top: 2px solid; border-bottom: 2px solid">
+ * 		 	  	<td>날짜</td>
+ * 		 	  	<td>작성자</td>
+ * 		 	  	<td>변경내용</td>
+ * 		 	</tr>
+ * 		</thead>
+ * 		<tbody>
+ * 		 	<tr style="border-bottom: 1px dotted">
+ * 		 	  <td>26. 3. 6</td>
+ * 		 	  <td>areum Jang</td>
+ * 		 	  <td>최초 생성 (버전 2.0)</td>
+ * 		 	</tr>
+ * 		</tbody>
+ * </table>
+ */
+@Getter
+public enum ServiceAction {
+	LOGIN("로그인"),
+	LEDGER_REGISTER("가계부 등록");
+
+	private final String title;
+
+	ServiceAction(String title) {
+		this.title = title;
+	}
+}
